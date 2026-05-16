40d8cd00762ba13458544405862d0be3c9f9ae8c
Oualidelaz <gthb112000@gmail.com>
2026-05-16 15:55:50 +0000

    Extract all IP addresses from a text file

---
+++ Day_1_IP_Addresses_Subnetting/3.py
@@ -0,0 +1,19 @@
+from ipaddress import ip_address
+from ntpath import isfile
+from pathlib import Path
+
+def extractIPs():
+    path = Path("file.txt")
+    if path.is_file():
+        IPs = []
+        with open(path, "r") as f:
+            content = f.readlines()
+            for line in content:
+                try:
+                    ip_address(line.strip())
+                    IPs.append(line.strip())
+                except ValueError:
+                    continue
+            return IPs
+    else:
+        return "File Not Found!"
